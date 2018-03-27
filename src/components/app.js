7de9b4d5b81b4afa8f33c340a775f142162ba1fd
neilmyers <mmyersneil@gmail.com>
2018-03-26 19:41:31 -0600

    Added .gitignore and changed h1 tag in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>More React Practice</h1>
       </div>
     );
   }
